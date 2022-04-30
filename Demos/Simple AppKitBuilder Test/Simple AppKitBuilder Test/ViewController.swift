7f61159f82f82c35c648f6765319a1c6160832e3
Darren Ford <dford_au-reg@yahoo.com>
2022-04-30 10:02:36 +1000

    Fixed demo build

--- Demos/Simple AppKitBuilder Test/Simple AppKitBuilder Test/ViewController.swift
+++ Demos/Simple AppKitBuilder Test/Simple AppKitBuilder Test/ViewController.swift
@@ -25,18 +25,17 @@ class AppKitLayoutDemoContainer: NSObject, DSFAppKitBuilderViewHandler {
 	let truncatingText = "This is the description that can be quite long and will be truncated when not enough space is available"
 	let wrappingText = "This is the description that can be quite long and will be wrapped when not enough space is available"
 	lazy var body: Element =
-		ZStack {
-			ZLayer(layoutType: .center) {
-				VStack {
-					IdentityElement(truncatingText, .byTruncatingTail)
-					IdentityElement(wrappingText, .byWordWrapping)
-				}
+	ZStack {
+		ZLayer(layoutType: .center) {
+			VStack {
+				IdentityElement(truncatingText, .byTruncatingTail)
+				IdentityElement(wrappingText, .byWordWrapping)
 			}
 		}
+	}
 }
 
 class IdentityElement: Element {
-
 	let identityDescription: String
 	let lineBreakMode: NSLineBreakMode
 
@@ -52,24 +51,25 @@ class IdentityElement: Element {
 	override func view() -> NSView { return self.body.view() }
 
 	lazy var body: Element =
-		HStack(spacing: 4) {
-			ImageView()
-				.image(NSImage(named: "apple_logo_orig")!)             // The image
-				.size(width: 42, height: 42, priority: .required)      // fixed size
-			VStack(spacing: 2, alignment: .leading) {
-				Label("Apple Computer")                                // The label with title 'Name'
-					.font(NSFont.systemFont(ofSize: 24))                // Font size 12
-					.lineBreakMode(.byTruncatingTail)                   // Truncate line
-					.horizontalPriorities(compressionResistance: 100)   // Allow the text field to compress
-				Label(identityDescription)                             // The description label
-					.font(NSFont.systemFont(ofSize: 12))                // Font size 12
-					.textColor(.placeholderTextColor)                   // Grey text
-					.lineBreakMode(lineBreakMode)                       // Line break mode
-					.horizontalPriorities(compressionResistance: 250)   // Allow the text field to compress
-			}
+	HStack(spacing: 4) {
+		ImageView()
+			.image(NSImage(named: "apple_logo_orig")!)           // The image
+			.size(width: 42, height: 42, priority: .required)    // fixed size
+		VStack(spacing: 2, alignment: .leading) {
+			Label("Apple Computer")                              // The label with title 'Name'
+				.font(NSFont.systemFont(ofSize: 24))              // Font size 12
+				.lineBreakMode(.byTruncatingTail)                 // Truncate line
+				.horizontalPriorities(compressionResistance: 100) // Allow the text field to compress
+			Label(identityDescription)                           // The description label
+				.font(NSFont.systemFont(ofSize: 12))              // Font size 12
+				.textColor(.placeholderTextColor)                 // Grey text
+				.lineBreakMode(lineBreakMode)                     // Line break mode
+				.horizontalPriorities(compressionResistance: 250) // Allow the text field to wrap
 		}
-		.edgeInsets(8)
-		.border(width: 0.5, color: .textColor)
-		.backgroundColor(.quaternaryLabelColor)
-		.cornerRadius(4)
+		.edgeInsets(6)
+	}
+	.edgeInsets(8)
+	.border(width: 0.75, color: .textColor)
+	.backgroundColor(.quaternaryLabelColor)
+	.cornerRadius(4)
 }
