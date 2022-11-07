3edb43a9490ff4e55aed66fbcb3538953d22c86b
Darren Ford <dford_au-reg@yahoo.com>
2022-11-08 09:08:02 +1100

    Explicitly define library types

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -9,17 +8,16 @@ let package = Package(
 		.macOS(.v10_11)
 	],
 	products: [
-		.library(name: "DSFAppKitBuilder", type: .static, targets: ["DSFAppKitBuilder"]),
+		.library(name: "DSFAppKitBuilder", targets: ["DSFAppKitBuilder"]),
+		.library(name: "DSFAppKitBuilder-static", type: .static, targets: ["DSFAppKitBuilder"]),
 		.library(name: "DSFAppKitBuilder-shared", type: .dynamic, targets: ["DSFAppKitBuilder"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/dagronf/DSFPagerControl", from: "2.0.0"),
-		.package(url: "https://github.com/dagronf/DSFMenuBuilder", from: "1.0.0"),
+		.package(url: "https://github.com/dagronf/DSFPagerControl", from: "2.2.0"),
+		.package(url: "https://github.com/dagronf/DSFMenuBuilder", from: "1.2.0"),
 		.package(url: "https://github.com/dagronf/DSFValueBinders", from: "0.7.1"),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
-		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "DSFAppKitBuilder",
 			dependencies: ["DSFMenuBuilder", "DSFPagerControl", "DSFValueBinders"]),
