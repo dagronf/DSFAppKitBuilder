e37444ac82d8ba236e7ad67307023b4eb294423e
Darren Ford <dford_au-reg@yahoo.com>
2023-01-30 13:23:40 +1100

    Cleaned up Slider, added SwiftUI preview

--- Sources/DSFAppKitBuilder/components/DSFAppKitBuilder+Slider.swift
+++ Sources/DSFAppKitBuilder/components/DSFAppKitBuilder+Slider.swift
@@ -43,10 +43,10 @@ public class Slider: Control {
 	///   - value: The initial value for the slider
 	///   - isVertical: A bool indicating the orientation (horizontal or vertical) of the slider.
 	public init(
-		range: ClosedRange<Double> = 0...100,
+		range: ClosedRange<Double> = 0 ... 100,
 		value: Double = 20,
-		isVertical: Bool)
-	{
+		isVertical: Bool = false
+	) {
 		super.init()
 
 		self.slider.minValue = range.lowerBound
@@ -58,33 +58,16 @@ public class Slider: Control {
 		self.slider.isVertical = isVertical
 	}
 
-	/// Create a slider element
-	/// - Parameters:
-	///   - range: The range of the slider
-	///   - value: The initial value for the slider
-	public init(
-		range: ClosedRange<Double> = 0...100,
-		value: Double = 20)
-	{
-		super.init()
-
-		self.slider.minValue = range.lowerBound
-		self.slider.maxValue = range.upperBound
-		self.slider.doubleValue = value
-
-		self.slider.target = self
-		self.slider.action = #selector(sliderDidChange(_:))
-	}
-
 	deinit {
 		self.valueBinder?.deregister(self)
+		self.actionCallback = nil
 	}
 
 	// Private
 	private let slider = NSSlider()
-	public override func view() -> NSView { return self.slider }
+	override public func view() -> NSView { return self.slider }
 	private var valueBinder: ValueBinder<Double>?
-	private var actionCallback: ((Double) -> Void)? = nil
+	private var actionCallback: ((Double) -> Void)?
 }
 
 // MARK: - Modifiers
@@ -107,7 +90,6 @@ public extension Slider {
 // MARK: - Actions
 
 public extension Slider {
-
 	/// Set a callback block for when the selection changes
 	func onChange(_ block: @escaping (Double) -> Void) -> Self {
 		self.actionCallback = block
@@ -137,3 +119,81 @@ public extension Slider {
 		return self
 	}
 }
+
+// MARK: - SwiftUI preview
+
+#if DEBUG && canImport(SwiftUI)
+import SwiftUI
+
+private let _value1 = ValueBinder(40.0)
+private let _value2 = ValueBinder(86.0)
+private let _value3 = ValueBinder(10.0)
+private let _numberFormatter: NumberFormatter = {
+	let n = NumberFormatter()
+	n.maximumFractionDigits = 2
+	n.minimumFractionDigits = 2
+	return n
+}()
+
+@available(macOS 10.15, *)
+struct SliderPreviews: PreviewProvider {
+	static var previews: some SwiftUI.View {
+		SwiftUI.VStack {
+			VStack {
+				Grid {
+					GridRow {
+						Label("continuous (regular)")
+						Slider(range: 0 ... 100, value: 10)
+							.bindValue(_value1)
+						Label(_value1.stringValue())
+							.formatter(_numberFormatter)
+							.width(50)
+					}
+					GridRow {
+						Label("continuous (small)")
+						Slider(range: 0 ... 100, value: 10)
+							.bindValue(_value1)
+							.controlSize(.small)
+							.trackColor(.systemRed)
+						Label(_value1.stringValue())
+							.formatter(_numberFormatter)
+							.width(50)
+					}
+					GridRow {
+						Label("continuous (mini)")
+						Slider(range: 0 ... 100, value: 10)
+							.bindValue(_value1)
+							.controlSize(.mini)
+							.trackColor(.systemPurple)
+						Label(_value1.stringValue())
+							.formatter(_numberFormatter)
+							.width(50)
+					}
+					GridRow {
+						Label("show tick marks")
+						Slider(range: 0 ... 100, value: 10)
+							.bindValue(_value2)
+							.numberOfTickMarks(10)
+						Label(_value2.stringValue())
+							.formatter(_numberFormatter)
+							.width(50)
+					}
+					GridRow {
+						Label("stop on tick marks")
+						Slider(range: 0 ... 100, value: 10)
+							.bindValue(_value3)
+							.numberOfTickMarks(11, allowsTickMarkValuesOnly: true)
+						Label(_value3.stringValue())
+							.formatter(_numberFormatter)
+							.width(50)
+					}
+				}
+				.columnFormatting(xPlacement: .trailing, atColumn: 0)
+				EmptyView()
+			}
+			.SwiftUIPreview()
+			.padding()
+		}
+	}
+}
+#endif
