afed0c4d080e54a8de287bff541221fa529c92e3
Darren Ford <dford_au-reg@yahoo.com>
2023-03-10 11:12:46 +1100

    Added AppKitFocusOverlay to demo

--- Demos/AppKit DSL Demo/AppKitBuilder TestBed/AppDelegate.swift
+++ Demos/AppKit DSL Demo/AppKitBuilder TestBed/AppDelegate.swift
@@ -8,14 +8,15 @@
 import Cocoa
 import DSFAppKitBuilder
 
+import AppKitFocusOverlay
+let _globalFocusOverlay = AppKitFocusOverlay()
+
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-	
-
-
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
+		_ = _globalFocusOverlay
 		DSFAppKitBuilderShowDebuggingOutput = true
 	}
 
